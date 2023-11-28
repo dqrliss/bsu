66c885354794abf92673370ec9eef1b60eb7b8db
victoria bodina <137838303+dqrliss@users.noreply.github.com>
2023-11-28 20:38:39 +0300

    Delete industrial programming/lаb4 directory

--- industrial programming/lаb4/src/my_comp.java
+++
@@ -1,6 +0,0 @@
-import java.util.Comparator;
-public class my_comp implements Comparator<st_recordbook> {
-    public int compare(st_recordbook a, st_recordbook b){
-        return a.get_name().compareTo(b.get_name()); // minus for int
-    }
-}
