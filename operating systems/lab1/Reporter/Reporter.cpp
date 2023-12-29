c058188b9d778d843f3ede3fd779720e228d5a3c
victoria bodina <137838303+dqrliss@users.noreply.github.com>
2023-12-30 01:26:19 +0300

    code readability has been improved

--- operating systems/lab1/Reporter/Reporter.cpp
+++ operating systems/lab1/Reporter/Reporter.cpp
@@ -14,14 +14,14 @@ int main(int argc, char* argv[]) {
     is.read(reinterpret_cast<char*>(emp), sizeof(employee) * quantity_of_employees);
     is.close();
 
-    std::sort(emp, emp + quantity_of_employees, [](employee& emp_1, employee& emp_2) -> bool {
-        return emp_1.num < emp_2.num; });
+    std::sort(emp, emp + quantity_of_employees, [](employee& emp_1, employee& emp_2)
+        -> bool {return emp_1.num < emp_2.num;});
 
     std::ofstream os(argv[3], std::ios::out);
     os << "report on the file " << argv[2] << '\n';
     for (int i = 0; i < quantity_of_employees; i++)
-        os << emp[i].num << std::setw(10) << emp[i].name << std::setw(10) << emp[i].hours
-        << std::setw(10) << emp[i].hours * atoi(argv[1]) << '\n';
+        os << emp[i].num << std::setw(10) << emp[i].name << std::setw(10)
+            << emp[i].hours << std::setw(10) << emp[i].hours * atoi(argv[1]) << '\n';
     os.close();
     delete[] emp;
 
