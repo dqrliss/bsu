42d5cf926436be199db92ea24eef3d7929e33a03
victoria bodina <137838303+dqrliss@users.noreply.github.com>
2023-12-30 01:11:45 +0300

    Add files via upload

--- operating systems/lab1/Main/Main.cpp
+++ operating systems/lab1/Main/Main.cpp
@@ -3,19 +3,26 @@
 #include <fstream>
 #include <conio.h>
 #include <string>
+#include <iomanip>
+#include "../Creator/header.h"
 
-int main()
-{
-    char bin[10]; int size;
-    std::cout << "name of binary file and number of notes: "; std::cin >> bin >> size;
+int main() {
+    char name_of_binary_file[15];
+    std::cout << "name of binary file: ";
+    std::cin >> name_of_binary_file;
 
-    char for_creator[100];
-    wsprintfA(for_creator, "C:\\Users\\dq\\source\\repos\\lab1\\Main\\x64\\Debug\\Creator.exe %d %s", size, bin);
+    int quantity_of_employees;
+    std::cout << "quantity of employees: ";
+    std::cin >> quantity_of_employees;
 
+    char cmd_line_args_c[100]; //to creator
+    wsprintfA(cmd_line_args_c, "C:\\Users\\dq\\source\\repos\\lab1\\Main\\x64\\Debug\\Creator.exe %d %s",
+        quantity_of_employees, name_of_binary_file);
+    
     STARTUPINFO si;
     PROCESS_INFORMATION piApp;
     ZeroMemory(&si, sizeof(STARTUPINFO));
-    if (!CreateProcessA(nullptr, for_creator, nullptr, nullptr, FALSE,
+    if (!CreateProcessA(nullptr, cmd_line_args_c, nullptr, nullptr, FALSE,
         CREATE_NEW_CONSOLE, nullptr, nullptr, (LPSTARTUPINFOA)&si, &piApp)) {
         _cputs("The new process is not created.\n");
         _cputs("Check a name of the process.\n");
@@ -23,38 +30,57 @@ int main()
         _getch();
         return 0;
     }
-    _cputs("The Creator process is created.\n");
+
     WaitForSingleObject(piApp.hProcess, INFINITE);
     CloseHandle(piApp.hThread);
     CloseHandle(piApp.hProcess);
 
-    char txt[10]; int cost;
-    std::cout << "name of report file and payment by hour: ";
-    std::cin >> txt >> cost;
+    employee emp;
+    std::ifstream is;
+    is.open(name_of_binary_file, std::ios::binary | std::ios::in);
+    std::cout << "\nbinary file contents:\n";
+    for (int i = 0; i < quantity_of_employees; i++) {
+        is.read((char*)&emp, sizeof(employee));
+        std::cout << emp.num << std::setw(10) << emp.name << std::setw(10) << emp.hours << "\n";
+    }
+    is.close();
 
-    char for_reporter[100];
-    wsprintfA(for_reporter, "C:\\Users\\dq\\source\\repos\\lab1\\Main\\x64\\Debug\\Reporter.exe %d %s %s", cost, bin, txt);
+    char name_of_report_file[15];
+    std::cout << "\nname of report file: ";
+    std::cin >> name_of_report_file;
 
-    ZeroMemory(&si, sizeof(STARTUPINFO));
-    if (!CreateProcessA(nullptr, for_reporter, nullptr, nullptr, FALSE,
-        CREATE_NEW_CONSOLE, nullptr, nullptr, (LPSTARTUPINFOA)&si, &piApp)) {
+    int hourly_wage;
+    std::cout << "hourly wage: ";
+    std::cin >> hourly_wage;
+    std::cout << '\n';
+
+    char cmd_line_args_r[100]; //to reporter
+    wsprintfA(cmd_line_args_r, "C:\\Users\\dq\\source\\repos\\lab1\\Main\\x64\\Debug\\Reporter.exe %d %s %s",
+        hourly_wage, name_of_binary_file, name_of_report_file);
+
+    STARTUPINFO si_;
+    PROCESS_INFORMATION piApp_;
+    ZeroMemory(&si_, sizeof(STARTUPINFO));
+    if (!CreateProcessA(nullptr, cmd_line_args_r, nullptr, nullptr, FALSE,
+        CREATE_NEW_CONSOLE, nullptr, nullptr, (LPSTARTUPINFOA)&si_, &piApp_)) {
         _cputs("The new process is not created.\n");
         _cputs("Check a name of the process.\n");
         _cputs("Press any key to finish.\n");
         _getch();
         return 0;
     }
-    _cputs("The Reporter process is created.\n");
-    WaitForSingleObject(piApp.hProcess, INFINITE);
-    CloseHandle(piApp.hThread);
-    CloseHandle(piApp.hProcess);
 
-    std::ifstream is(txt);
+    WaitForSingleObject(piApp_.hProcess, INFINITE);
+    CloseHandle(piApp_.hThread);
+    CloseHandle(piApp_.hProcess);
+
+    is.open(name_of_report_file, std::ios::in);
     std::string line;
-    while (!is.eof())
-    {
+    for (int i = 0; i < quantity_of_employees + 1; i++) {
         getline(is, line);
         std::cout << line << '\n';
     }
+    is.close();
+
     return 0;
 }
