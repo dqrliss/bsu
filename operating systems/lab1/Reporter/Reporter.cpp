6923c9f04e1f1cf3719c809bf7ff2232dd6d93b2
victoria bodina <137838303+dqrliss@users.noreply.github.com>
2023-12-30 01:12:34 +0300

    Add files via upload

--- operating systems/lab1/Reporter/Reporter.cpp
+++ operating systems/lab1/Reporter/Reporter.cpp
@@ -1,14 +1,29 @@
 #include <iostream>
 #include <fstream>
+#include <algorithm>
+#include <iomanip>
 #include "..\Creator\header.h"
 
-int main(int argc, char* argv[])
-{
-    employee emp;
+int main(int argc, char* argv[]) {
     std::ifstream is(argv[2], std::ios::binary | std::ios::in);
-    std::ofstream os(argv[3]);
+    is.seekg(0, std::ios::end);
+    int quantity_of_employees = static_cast<int>(is.tellg() / sizeof(employee));
+    employee* emp = new employee[quantity_of_employees];
 
-    os << "report on file " << argv[2] << ":\n";
-    while (is.read((char*)&emp, sizeof(employee))) os << emp.num << "\t" << emp.name << "\t" << emp.hours << "\t" << atoi(argv[1]) * emp.hours << "\n";
+    is.seekg(std::ios::beg);
+    is.read(reinterpret_cast<char*>(emp), sizeof(employee) * quantity_of_employees);
+    is.close();
+
+    std::sort(emp, emp + quantity_of_employees, [](employee& emp_1, employee& emp_2) -> bool {
+        return emp_1.num < emp_2.num; });
+
+    std::ofstream os(argv[3], std::ios::out);
+    os << "report on the file " << argv[2] << '\n';
+    for (int i = 0; i < quantity_of_employees; i++)
+        os << emp[i].num << std::setw(10) << emp[i].name << std::setw(10) << emp[i].hours
+        << std::setw(10) << emp[i].hours * atoi(argv[1]) << '\n';
+    os.close();
+    delete[] emp;
+
     return 0;
 }
