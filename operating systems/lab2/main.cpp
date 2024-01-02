de38ad8541cc6cb6517f0687e6f24eb7a757faad
victoria bodina <137838303+dqrliss@users.noreply.github.com>
2024-01-03 01:40:49 +0300

    global variables removed

--- operating systems/lab2/main.cpp
+++ operating systems/lab2/main.cpp
@@ -1,76 +1,92 @@
-#include <iostream>
 #include <windows.h>
+#include <iostream>
 
-int n, avg = 0, ind_max = 0, ind_min = 0;
-int* arr;
 
-DWORD WINAPI min_max() {
-    int max = INT_MIN, min = INT_MAX;
-    for (int i = 0; i < n; i++) {
-        if (arr[i] > max) {
-            max = arr[i];
-            ind_max = i;
-        }
-        Sleep(7);
-        if (arr[i] < min) {
-            min = arr[i];
-            ind_min = i;
-        }
-        Sleep(7);
-    }
-    std::cout << "max value: " << max << "\nmin value: " << min << '\n';
-    return 0;
+LPVOID WINAPI min_max(int* arr) {
+	int max = INT_MIN, min = INT_MAX;
+	int ind_max = 6, ind_min = 6;
+	for (int i = 0; i < arr[0]; i++) {
+		if (max < arr[i + 6]) {
+			max = arr[i + 6];
+			ind_max = i + 6;
+		}
+		Sleep(7);
+		if (min > arr[i + 6]) {
+			min = arr[i + 6];
+			ind_min = i + 6;
+		}
+		Sleep(7);
+	}
+	arr[1] = min, arr[2] = max, arr[3] = ind_min, arr[4] = ind_max;
+
+	std::cout << "max value: " << max << "\nmin value: " << min << '\n';
+
+	return 0;
 }
 
-DWORD WINAPI average() {
-    for (int i = 0; i < n; i++) {
-        avg += arr[i];
-        Sleep(12);
-    }
-    avg /= n;
-    std::cout << "\naverage value: " << avg << '\n';
-    return 0;
+DWORD WINAPI average(int* arr) {
+	int avg = 0;
+	for (int i = 0; i < arr[0]; i++) {
+		avg += arr[i + 6];
+		Sleep(12);
+	}
+	arr[5] = avg / arr[0];
+
+	std::cout << "\naverage value: " << arr[5] << '\n';
+
+	return 0;
 }
 
 int main() {
-    try {
-        std::cout << "array size: ";
-        std::cin >> n;
+	try {
+	int n, max, min, ind_max, ind_min, avg;
 
-        if (n == 0) throw std::exception("division by zero in average()");
-        arr = new int[n];
-        std::cout << "array elements: ";
-        for (int i = 0; i < n; i++)
-            std::cin >> arr[i];
+	std::cout << "array size: ";
+	std::cin >> n;
 
-        HANDLE hThread, hThread_;
-        DWORD IDThread, IDThread_;
+	if (n == 0) throw std::exception("division by zero in average()");
 
-        hThread = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)min_max,
-            NULL, 0, &IDThread);
-        if (hThread == NULL)
-            return GetLastError();
+	int* arr = new int[n + 6];
+	arr[0] = n;
 
-        hThread_ = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)average,
-            NULL, 0, &IDThread_);
-        if (hThread_ == NULL)
-            return GetLastError();
+	std::cout << "array elements: ";
+	for (int i = 0; i < n; i++)
+		std::cin >> arr[i + 6];
 
-        WaitForSingleObject(hThread, INFINITE);
-        WaitForSingleObject(hThread_, INFINITE);
+	HANDLE hThread, hThread_;
+	DWORD IDThread, IDThread_;
 
-        CloseHandle(hThread_);
-        CloseHandle(hThread);
+	hThread = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)min_max,
+		(void*)arr, 0, &IDThread);
+	if (hThread == NULL)
+		return GetLastError();
 
-        arr[ind_max] = avg;
-        arr[ind_min] = avg;
-        std::cout << "array element which was the maximum: " << arr[ind_max] << '\n'
-            << "array element which was the minimum: " << arr[ind_min];
-        delete[] arr;
-    }
-    catch (std::exception& e) {
-        std::cout << e.what();
-    }
+	hThread_ = CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)average,
+		(void*)arr, 0, &IDThread_);
+	if (hThread_ == NULL)
+		return GetLastError();
 
-    return 0;
+	WaitForSingleObject(hThread, INFINITE);
+	WaitForSingleObject(hThread_, INFINITE);
+	
+	avg = arr[5];
+	ind_max = arr[4];
+	ind_min = arr[3];
+	arr[ind_max] = avg;
+	arr[ind_min] = avg;
+
+	for (int i = 0; i < n; i++)
+		std::cout << arr[i + 6] << " ";
+
+	CloseHandle(hThread_);
+	CloseHandle(hThread);
+
+	delete[] arr;
+
+	}
+	catch (std::exception& e) {
+		std::cout << e.what();
+	}
+
+	return 0;
 }
