783bff8ef879064cd060b332d736f4184990c4cd
victoria bodina <137838303+dqrliss@users.noreply.github.com>
2024-01-12 23:07:47 +0300

    code partly has been put into functions for making tests

--- industrial programming/lab2/lab2.1/src/Main.java
+++ industrial programming/lab2/lab2.1/src/Main.java
@@ -1,11 +1,18 @@
-import java.nio.file.Paths;
-import java.util.*;
 import java.io.*;
+import java.nio.file.Paths;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        Scanner in = new Scanner(Paths.get("input.txt"));
-        boolean flag = true;
+        int[][] matrix = readMatrixFromFile("input.txt");
+        boolean isSymmetrical = isSymmetrical(matrix);
+        swapMaxWithCenter(matrix);
+
+        writeMatrixToFile("output.txt", matrix, isSymmetrical);
+    }
+
+    private static int[][] readMatrixFromFile(String fileName) throws IOException {
+        Scanner in = new Scanner(Paths.get(fileName));
         int n = in.nextInt();
         int[][] matrix = new int[n][n];
         while (in.hasNextInt()) {
@@ -16,34 +23,54 @@ public class Main {
             }
         }
         in.close();
+        return matrix;
+    }
+
+    private static boolean isSymmetrical(int[][] matrix) {
+        int n = matrix.length;
         for (int i = 0; i < n; i++) {
             for (int j = i + 1; j < n; j++) {
                 if (matrix[i][j] != matrix[j][i]) {
-                    flag = false;
-                    break;
+                    return false;
                 }
             }
         }
+        return true;
+    }
+
+    private static void swapMaxWithCenter(int[][] matrix) {
+        int n = matrix.length;
         int max = matrix[0][0];
-        int max_row = 0; int max_column = 0;
+        int maxRow = 0;
+        int maxColumn = 0;
+
         for (int i = 0; i < n; i++) {
             if (matrix[i][i] > max) {
                 max = matrix[i][i];
-                max_row = i; max_column = i;
+                maxRow = i;
+                maxColumn = i;
             }
             if (matrix[i][n - i - 1] > max) {
                 max = matrix[i][n - i - 1];
-                max_row = i; max_column = n - i - 1;
+                maxRow = i;
+                maxColumn = n - i - 1;
             }
         }
-        int temp = matrix[max_row][max_column];
-        matrix [max_row][max_column] = matrix[n / 2][n / 2];
-        matrix [n / 2][n / 2] = temp;
-        PrintWriter out = new PrintWriter(new FileWriter("output.txt"));
-        if (flag) out.println("symmetrical matrix");
-        else out.println("non-symmetrical matrix");
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
+
+        int temp = matrix[maxRow][maxColumn];
+        matrix[maxRow][maxColumn] = matrix[n / 2][n / 2];
+        matrix[n / 2][n / 2] = temp;
+    }
+
+    private static void writeMatrixToFile(String fileName, int[][] matrix, boolean isSymmetrical) throws IOException {
+        PrintWriter out = new PrintWriter(new FileWriter(fileName));
+        if (isSymmetrical) {
+            out.println("symmetrical matrix");
+        } else {
+            out.println("non-symmetrical matrix");
+        }
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
                 out.print(matrix[i][j] + " ");
             }
             out.println();
