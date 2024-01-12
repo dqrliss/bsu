632c88d9b10ca463a5373b8bd4e171bee4cbc31a
victoria bodina <137838303+dqrliss@users.noreply.github.com>
2024-01-12 23:24:10 +0300

    code partly has been put into functions for making tests

--- industrial programming/lab2/lab2.3/src/Main.java
+++ industrial programming/lab2/lab2.3/src/Main.java
@@ -1,16 +1,28 @@
-import java.nio.file.Paths;
-import java.util.*;
 import java.io.*;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.Scanner;
 import java.util.regex.Pattern;
+
 public class Main {
     public static void main(String[] args) throws IOException {
         Scanner in = new Scanner(Paths.get("input.txt"));
         int n = Integer.parseInt(in.nextLine());
+        int[][] matrix = readMatrixFromFile(n, in);
+        in.close();
+
+        int[][] dp = countLocalMaxima(matrix);
+        int minLocalMaxima = findMinLocalMaxima(matrix, dp);
+
+        writeResultToFile("output.txt", minLocalMaxima);
+    }
+
+    public static int[][] readMatrixFromFile(int n, Scanner in) {
         int[][] matrix = new int[n + 1][n + 1];
-        for(int i = 0; i < n + 1; i++) {
+        for (int i = 0; i < n + 1; i++) {
             matrix[i][0] = Integer.MIN_VALUE;
         }
-        for(int i = 0; i < n + 1; i++) {
+        for (int i = 0; i < n + 1; i++) {
             matrix[0][i] = Integer.MIN_VALUE;
         }
         Pattern pattern = Pattern.compile("\\s");
@@ -20,17 +32,15 @@ public class Main {
                 matrix[i][j] = Integer.parseInt(row[j - 1]);
             }
         }
-        in.close();
+        return matrix;
+    }
+
+    public static int[][] countLocalMaxima(int[][] matrix) {
+        int n = matrix.length - 1;
         int[][] dp = new int[n + 1][n + 1];
-        for(int i = 0; i <= n; i++) {
-            dp[i][0] = 0;
-        }
-        for(int i = 0; i <= n; i++) {
-            dp[0][i] = 0;
-        }
-        int min = Integer.MAX_VALUE;
-        for (int i = 1; i < n + 1; i++) {
-            for (int j = 1; j < n + 1; j++) {
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
                 if (j == n && i == n) {
                     if (dp[i][j - 1] == 1 || dp[i - 1][j - 1] == 1 || dp[i - 1][j] == 1) {
                         dp[i][j] = 0;
@@ -38,7 +48,6 @@ public class Main {
                     }
                     if (matrix[i][j] > matrix[i - 1][j - 1] && matrix[i][j] > matrix[i - 1][j] && matrix[i][j] > matrix[i][j - 1]) {
                         dp[i][j] = 1;
-                        if (matrix[i][j] < min) min = matrix[i][j];
                         continue;
                     }
                     dp[i][j] = 0;
@@ -51,7 +60,6 @@ public class Main {
                     }
                     if (matrix[i][j] > matrix[i - 1][j - 1] && matrix[i][j] > matrix[i - 1][j] && matrix[i][j] > matrix[i][j - 1] && matrix[i][j] > matrix[i + 1][j - 1] && matrix[i][j] > matrix[i + 1][j]) {
                         dp[i][j] = 1;
-                        if (matrix[i][j] < min) min = matrix[i][j];
                         continue;
                     }
                     dp[i][j] = 0;
@@ -64,7 +72,6 @@ public class Main {
                     }
                     if (matrix[i][j] > matrix[i - 1][j - 1] && matrix[i][j] > matrix[i - 1][j] && matrix[i][j] > matrix[i][j - 1] && matrix[i][j] > matrix[i - 1][j + 1] && matrix[i][j] > matrix[i][j + 1]) {
                         dp[i][j] = 1;
-                        if (matrix[i][j] < min) min = matrix[i][j];
                         continue;
                     }
                     dp[i][j] = 0;
@@ -76,21 +83,35 @@ public class Main {
                 }
                 if (matrix[i][j] > matrix[i - 1][j - 1] && matrix[i][j] > matrix[i - 1][j] && matrix[i][j] > matrix[i - 1][j + 1] && matrix[i][j] > matrix[i][j - 1] && matrix[i][j] > matrix[i][j + 1] && matrix[i][j] > matrix[i + 1][j - 1] && matrix[i][j] > matrix[i + 1][j] && matrix[i][j] > matrix[i + 1][j + 1]) {
                     dp[i][j] = 1;
-                    if (matrix[i][j] < min) min = matrix[i][j];
                     continue;
                 }
                 dp[i][j] = 0;
             }
         }
-        for (int i = 1; i < n + 1; i++) {
-            for (int j = 1; j < n + 1; j++) {
-                System.out.print(dp[i][j] + " ");
+        return dp;
+    }
+
+    public static int findMinLocalMaxima(int[][] matrix, int[][] dp) {
+        int minLocalMaxima = Integer.MAX_VALUE;
+        int n = matrix.length - 1;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (dp[i][j] == 1 && matrix[i][j] < minLocalMaxima) {
+                    minLocalMaxima = matrix[i][j];
+                }
             }
-            System.out.println();
         }
-        PrintWriter out = new PrintWriter(new FileWriter("output.txt"));
-        if (min == Integer.MAX_VALUE) out.print("there are no local maxima");
-        else out.print(min);
+        return minLocalMaxima;
+    }
+
+    public static void writeResultToFile(String fileName, int minLocalMaxima) throws IOException {
+        PrintWriter out = new PrintWriter(new FileWriter(fileName));
+        if (minLocalMaxima == Integer.MAX_VALUE) {
+            out.print("there are no local maxima");
+        } else {
+            out.print(minLocalMaxima);
+        }
         out.close();
     }
 }
