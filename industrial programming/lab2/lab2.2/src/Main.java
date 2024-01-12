4b23d98db30f2ebd547532b03fc1003047d17488
victoria bodina <137838303+dqrliss@users.noreply.github.com>
2024-01-12 23:20:55 +0300

    code partly has been put into functions for making tests

--- industrial programming/lab2/lab2.2/src/Main.java
+++ industrial programming/lab2/lab2.2/src/Main.java
@@ -1,11 +1,24 @@
-import java.nio.file.Paths;
-import java.util.*;
 import java.io.*;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         Scanner in = new Scanner(Paths.get("input.txt"));
         int n = Integer.parseInt(in.nextLine());
+        int[][] matrix = readMatrixFromFile(n, in);
+        in.close();
+        
+        Map<Integer, Integer> occurrences = countOccurrences(matrix);
+        int maxDuplicate = findMaxDuplicateNumber(occurrences);
+        
+        writeMaxDuplicateNumberToFile("output.txt", maxDuplicate);
+    }
+
+    public static int[][] readMatrixFromFile(int n, Scanner in) {
         int[][] matrix = new int[n][n];
         for (int i = 0; i < n; i++) {
             StringTokenizer tokenizer = new StringTokenizer(in.nextLine());
@@ -13,21 +26,37 @@ public class Main {
                 matrix[i][j] = Integer.parseInt(tokenizer.nextToken());
             }
         }
-        in.close();
-        Map <Integer, Integer> q = new HashMap<>();
-        int max = Integer.MIN_VALUE;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                int counter = q.getOrDefault(matrix[i][j], 0) + 1;
-                q.put(matrix[i][j], counter);
-                if (counter > 1 && matrix[i][j] > max) {
-                    max = matrix[i][j];
-                }
+        return matrix;
+    }
+
+    public static Map<Integer, Integer> countOccurrences(int[][] matrix) {
+        Map<Integer, Integer> occurrences = new HashMap<>();
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                int counter = occurrences.getOrDefault(matrix[i][j], 0) + 1;
+                occurrences.put(matrix[i][j], counter);
             }
         }
-        PrintWriter out = new PrintWriter(new FileWriter("output.txt"));
-        if (max == Integer.MIN_VALUE) out.print("there are no such numbers");
-        else out.print(max);
+        return occurrences;
+    }
+
+    public static int findMaxDuplicateNumber(Map<Integer, Integer> occurrences) {
+        int maxDuplicate = 0;
+        for (Map.Entry<Integer, Integer> entry : occurrences.entrySet()) {
+            if (entry.getValue() > 1 && entry.getKey() > maxDuplicate) {
+                maxDuplicate = entry.getKey();
+            }
+        }
+        return maxDuplicate;
+    }
+
+    public static void writeMaxDuplicateNumberToFile(String fileName, int maxDuplicate) throws IOException {
+        PrintWriter out = new PrintWriter(new FileWriter(fileName));
+        if (maxDuplicate == 0) {
+            out.print("there are no such numbers");
+        } else {
+            out.print(maxDuplicate);
+        }
         out.close();
     }
 }
