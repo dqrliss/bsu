f1b1b683e691cd07093a58f1c9958daebe052a3a
victoria bodina <137838303+dqrliss@users.noreply.github.com>
2024-01-12 23:26:54 +0300

    code partly has been put into functions for making tests

--- industrial programming/lab3/src/Main.java
+++ industrial programming/lab3/src/Main.java
@@ -1,20 +1,27 @@
 import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) throws NumberFormatException {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String line = scanner.nextLine();
-        String str = line.replaceAll("[^a-zA-Z]", "");
+        processString(line);
+        scanner.close();
+    }
+
+    public static void processString(String input) {
+        String str = input.replaceAll("[^a-zA-Z]", "");
         char[] charArray = str.toCharArray();
-        for (char c: charArray) {
+
+        for (char c : charArray) {
             System.out.print(c + "  ");
         }
         System.out.println();
+
         for (char c : charArray) {
             int ascii = c;
             if (ascii < 95) ascii -= 64;
             else ascii -= 96;
             System.out.print(ascii + "  ");
         }
-        scanner.close();
     }
 }
